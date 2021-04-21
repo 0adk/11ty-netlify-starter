f5b3bb678e6c0b74287b53215d8abe46b6748ed8
Adrianna Kolosa <a.kolosa@infinity-group.pl>
2021-04-21 12:58:28 +0200

    admin config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
   config.addPassthroughCopy({ 'src/assets/scripts': 'js' });
   config.addPassthroughCopy({ 'src/assets/images': 'img' });
+  config.addPassthroughCopy({ 'src/admin': 'admin' })
 
   return {
     dir: {
