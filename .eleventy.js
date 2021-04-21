cca166abe98b21f62ca50a73aff2bc2a06c3d18c
Adrianna Kolosa <a.kolosa@infinity-group.pl>
2021-04-21 16:25:14 +0200

    data folder location

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,12 @@ module.exports = function (config) {
   config.addPassthroughCopy({ 'src/assets/scripts': 'assets/js' });
   config.addPassthroughCopy({ 'src/assets/images': 'assets/img' });
   config.addPassthroughCopy({ 'src/admin': 'admin' })
-  config.addPassthroughCopy({ 'src/_data': 'data' })
 
   return {
     dir: {
       input: 'src',
       output: 'public',
+      data: "../data"
     },
   };
 };
--- src/_data/posts/first-post.md
+++
@@ -1,8 +0,0 @@
----
-layout: layouts/post.njk
-title: My first post
-description: The first post on my blog
-date: 2020-04-25
-featuredImage: /images/uploads/image1.jpg
----
-Hello, here is the body of the post.
